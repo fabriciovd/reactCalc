fbee1d4ffb7b507bf415bd993fe25e8921d154f4
fabriciovd <YavorPetkov@students.softuni.bg>
2024-07-02 13:51:55 +0300

    Updated the style of the calc

--- src/components/CalcScreen.jsx
+++ src/components/CalcScreen.jsx
@@ -1,7 +1,7 @@
 export default function CalcScreen(props) {
   return (
     <div className="calc-screen">
-      <div className="calc-operation">{props.history} </div>
+      <div className="calc-operation">React Calc</div>
       <div className="calc-typed">
         {props.text}<span className="blink-me">_</span>
       </div>
